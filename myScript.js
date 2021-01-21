8687db032397ec3eb5a67331ef5bbae6b97c8e8d
Jony Zakari <62159789+Corioco-jony@users.noreply.github.com>
2021-01-21 02:18:37 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,10 @@
+let photoBox = document.getElementById('item1');
+
+//function show() {
+//item1.addEventListener('click', photoBox);
+//}
+
+function increasedWidth() {
+    item1.style.width = '2000px';
+}
+item1.addEventListener('click', increasedWidth);
